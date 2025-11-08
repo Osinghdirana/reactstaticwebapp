f326455cac5ada180f9c101c0f3b1a7a9534ffc2
Osinghdirana <digamber.singh@sparsolutions.com>
2025-11-08 19:31:54 +0530

    Update text color and add question mark

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div style={{ fontFamily: 'system-ui', textAlign: 'center', margin: '80px' }}>
       <h1>Website coming Soon</h1>
       <p>
-        <strong>Hello my wife Mamta, I love you. Do you love me</strong>
+        <strong style={{ color: 'red' }}>Hello my wife Mamta, I love you. Do you love me?</strong>
       </p>
     </div>
   );
