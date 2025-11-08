b8cb502daab4083466c6681560fa7a252e9c0d8b
Osinghdirana <digamber.singh@sparsolutions.com>
2025-11-08 19:28:13 +0530

    Update App component to display new message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function App() {
-  const value = 'World, Its me Digamber';
-  return <div>Hello {value}</div>;
+  return (
+    <div style={{ fontFamily: 'system-ui', textAlign: 'center', margin: '80px' }}>
+      <h1>Website coming Soon</h1>
+      <p>
+        <strong>Hello my wife Mamta, I love you. Do you love me</strong>
+      </p>
+    </div>
+  );
 }
 
 export default App;
